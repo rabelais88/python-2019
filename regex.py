79cb1a8ec30eecc74c12c48e6dc08f7e27df365b
sungryeol park(max) <sungryeolp@gmail.com>
2019-07-13 15:21:39 +0900

    update

---
+++ regex.py
@@ -0,0 +1,14 @@
+import re
+
+mystr = "I'm in the woods all alone"
+
+searched = re.search("woods", mystr)
+matched = re.match("woods", mystr)  # match is automatically anchored to the start
+
+result = {"searched": searched, "matched": matched}
+print(result["searched"])
+
+print(matched == None) # None object == null object in JS, java
+
+matched_two = re.search(r"WOODS", mystr, re.IGNORECASE)
+print(matched_two)
